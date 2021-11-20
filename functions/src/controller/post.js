a48241cb00e567baa254fd5c53ef4712f51355b1
holmir97 <holmir97@gmail.com>
2021-11-20 14:10:32 +0900

    feat: post controller

--- functions/src/controller/post.js
+++ functions/src/controller/post.js
@@ -1,3 +1,8 @@
+const util = require("../lib/util");
+const statusCode = require("../constants/statusCode");
+const responseMessage = require("../constants/responseMessage");
+
+const postService = require("../service/postService ");
 /**
  *  @포스트 전체 조회
  *  @route GET post/
@@ -6,7 +11,31 @@
 
 const postAllController = async (req, res) => {
   try {
-  } catch (err) {}
+    // 조회 성공
+    const data = await postService.getPostAll();
+
+    console.log(data);
+
+    // 디비 에러
+    if (data === -1) {
+      return res
+        .status(statusCode.DB_ERROR)
+        .send(util.fail(statusCode.DB_ERROR, "DB 에러"));
+    }
+    // response
+    res
+      .status(statusCode.OK)
+      .send(util.success(statusCode.OK, "포스트 전체 조회 성공", data));
+  } catch (error) {
+    return res
+      .status(statusCode.INTERNAL_SERVER_ERROR)
+      .send(
+        util.fail(
+          statusCode.INTERNAL_SERVER_ERROR,
+          responseMessage.INTERNAL_SERVER_ERROR
+        )
+      );
+  }
 };
 
 module.exports = {
