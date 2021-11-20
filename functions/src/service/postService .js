565e5356db5e159e6b6b73471477ac36b5f1e9db
holmir97 <holmir97@gmail.com>
2021-11-20 14:11:04 +0900

    feat: post service

--- functions/src/service/postService .js
+++ functions/src/service/postService .js
@@ -1,3 +1,10 @@
+const util = require("../lib/util");
+const statusCode = require("../constants/statusCode");
+const responseMessage = require("../constants/responseMessage");
+const functions = require("firebase-functions");
+const db = require("../db/db");
+const { postDB } = require("../db");
+
 /**
  *  @포스트 전체 조회
  *  @route Post /post/
@@ -5,7 +12,28 @@
  *  @error
  */
 
-const getPostAll = async (data) => {};
+const getPostAll = async (req) => {
+  let client;
+
+  try {
+    client = await db.connect(req);
+
+    const posts = await postDB.getAllPosts(client);
+
+    return posts;
+  } catch (error) {
+    functions.logger.error(
+      `[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`,
+      `[CONTENT] ${error}`
+    );
+    console.log(error);
+
+    // DB 에러
+    return -1;
+  } finally {
+    client.release();
+  }
+};
 
 module.exports = {
   getPostAll,
