d67fdd7fd84712cd9337e20f0847959549d54c07
holmir97 <holmir97@gmail.com>
2021-11-20 14:10:47 +0900

    feat: post router

--- functions/src/router/post.js
+++ functions/src/router/post.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/"); // 포스트 전체 조회
+const postController = require("../controller/post.js");
+
+router.get("/", postController.postAllController); // 포스트 전체 조회
 router.get("/search"); // 포스트 검색
 router.post("/upload"); // 포스트 업로드
 
