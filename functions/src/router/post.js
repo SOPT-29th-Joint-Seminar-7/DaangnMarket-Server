7e5153376d11b9569508ca170d6ce4c53a7462a0
chanwoo <chocw0402@gmail.com>
2021-11-22 20:39:43 +0900

    feat: postController 추가

--- functions/src/router/post.js
+++ functions/src/router/post.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const postController = require("../controller/post.js");
 
 router.get("/", postController.postAllController); // 포스트 전체 조회
-router.get("/search"); // 포스트 검색
+router.get("/search", postController.postSearchController); // 포스트 검색
 router.post("/upload"); // 포스트 업로드
 
 module.exports = router;
