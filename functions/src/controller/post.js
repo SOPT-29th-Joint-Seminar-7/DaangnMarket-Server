106537b385f23b8897ce9346a6d3d71e067f571c
holmir97 <holmir97@gmail.com>
2021-11-25 13:48:33 +0900

    feat: post upload api

--- functions/src/controller/post.js
+++ functions/src/controller/post.js
@@ -36,6 +36,57 @@ const postAllController = async (req, res) => {
   }
 };
 
+/**
+ *  @포스트 등록
+ *  @route Post /post/upload
+ *  @access public
+ *  @error
+ */
+
+const postUploadController = async (req, res) => {
+  try {
+    // 포스트 등록
+    const resData = await postService.postPostUpload(req);
+
+    // 디비 에러
+    if (resData === -1) {
+      return res
+        .status(statusCode.DB_ERROR)
+        .send(util.fail(statusCode.DB_ERROR, "DB 에러"));
+    }
+
+    // 요청 바디 부족
+    else if (resData === -2) {
+      return res
+        .status(statusCode.BAD_REQUEST)
+        .send(util.fail(statusCode.BAD_REQUEST, responseMessage.NULL_VALUE));
+    }
+
+    // 잘못된 요청 값
+    else if (resData === -3) {
+      return res
+        .status(statusCode.BAD_REQUEST)
+        .send(util.fail(statusCode.BAD_REQUEST, responseMessage.OUT_OF_VALUE));
+    }
+    // 포스트 등록 성공
+    else {
+      res
+        .status(statusCode.OK)
+        .send(util.success(statusCode.OK, "포스트 등록 성공"));
+    }
+  } catch (error) {
+    return res
+      .status(statusCode.INTERNAL_SERVER_ERROR)
+      .send(
+        util.fail(
+          statusCode.INTERNAL_SERVER_ERROR,
+          responseMessage.INTERNAL_SERVER_ERROR
+        )
+      );
+  }
+};
+
 module.exports = {
   postAllController,
+  postUploadController,
 };
--- functions/src/service/postService .js
+++ functions/src/service/postService .js
@@ -1,10 +1,13 @@
 const functions = require("firebase-functions");
 const db = require("../db/db");
-const { postDB } = require("../db");
+const postDB = require("../db/post");
+const userDB = require("../db/user");
+const img = require("../sampleDB/img");
+const imgs = require("../sampleDB/img");
 
 /**
  *  @포스트 전체 조회
- *  @route Post /post/
+ *  @route Get /post/
  *  @access public
  *  @error
  */
@@ -13,7 +16,7 @@ const getPostAll = async (req) => {
   let client;
 
   try {
-    client = await db.connect(req);
+    client = await db.connect();
 
     const posts = await postDB.getAllPosts(client);
 
@@ -32,6 +35,65 @@ const getPostAll = async (req) => {
   }
 };
 
+/**
+ *  @포스트 등록
+ *  @route Post /post/upload
+ *  @access public
+ *  @error
+ */
+
+const postPostUpload = async (req) => {
+  let client;
+
+  try {
+    client = await db.connect();
+
+    const { title, category, price, state, trade, content } = req.body;
+
+    // 요청 바디 부족
+    if (!title || !category || !price || !state || !trade || !content) {
+      return -2;
+    }
+
+    // price가 음수 값
+    if (price <= 0) {
+      return -3;
+    }
+
+    // 유저는 랜덤 유저 삽입
+    const users = await userDB.getAllUsers(client);
+    const userArr = Object.values(users);
+    const randomUser = Math.floor(Math.random() * userArr.length) + 1;
+
+    const selectUser = userArr.filter((user) => user.id === randomUser)[0];
+    const postInfo = {
+      title,
+      category,
+      price,
+      state,
+      trade,
+      content,
+      address: selectUser.address,
+      img: imgs[0],
+    };
+
+    // // DB Post 등록
+    await postDB.postPostUpload(client, postInfo, randomUser);
+  } catch (error) {
+    functions.logger.error(
+      `[ERROR] [${req.method.toUpperCase()}] ${req.originalUrl}`,
+      `[CONTENT] ${error}`
+    );
+    console.log(error);
+
+    // DB 에러
+    return -1;
+  } finally {
+    client.release();
+  }
+};
+
 module.exports = {
   getPostAll,
+  postPostUpload,
 };
