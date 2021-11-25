630044e8ec08e50e0fbae6b8b33992606583e82a
holmir97 <holmir97@gmail.com>
2021-11-25 13:51:54 +0900

    feat: default img

---
+++ functions/src/sampleDB/img.js
@@ -0,0 +1,3 @@
+module.exports = [
+  "https://firebasestorage.googleapis.com/v0/b/daangnmarket-wesopt.appspot.com/o/%EB%8B%B9%EA%B7%BC%EB%94%94%ED%8F%B4%ED%8A%B8.jpg?alt=media&token=c92358db-a42d-40cb-92c3-98d8518ff626",
+];
